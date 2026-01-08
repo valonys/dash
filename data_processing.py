a5159776b9e95077184888dcaaa42f34acaee8b1
Ataliba Miguel <ataliba.miguel@valonylabs.com>
2026-01-08 14:34:21 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -19,7 +19,7 @@ class DataProcessor:
        
    def load_inspection_data(self, uploaded_file, selected_site='GIR'):
        site_config = {
-           'GIR': {'nrows': 587},
+           'GIR': {'nrows': 582},
            'DAL': {'nrows': 761},
            'PAZ': {'nrows': 846}, 
            'CLV': {'nrows': 735}
@@ -240,4 +240,5 @@ class DataProcessor:
     
 
     return performance_data
+
 
